fd061060ca7d8346d713f2f14107d276630a7ca4
ModiSireesha <modisireesha09@gmail.com>
2024-09-16 15:14:36 +0530

    Created Maven project

---
+++ MavenS/src/main/java/com/spriMaven/MavenS/App.java
@@ -0,0 +1,13 @@
+package com.spriMaven.MavenS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
